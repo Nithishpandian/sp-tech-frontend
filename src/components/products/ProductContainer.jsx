6b587ba8a1f53611403a9f7391f8c956349b3adc
Nithishpandian <nithishpandiyan75@gmail.com>
2023-12-23 00:25:16 +0530

    design change

--- src/components/products/ProductContainer.jsx
+++ src/components/products/ProductContainer.jsx
@@ -3,20 +3,13 @@ import ProductCard from "./ProductCard";
 
 const ProductContainer = () => {
   return (
-    <div className="px-32 py-10 flex flex-col gap-1">
-      <div>
-        <h1 className=" text-gray text-4xl font-krona-one font-bold">
-          Products
-        </h1>
-      </div>
-      <div className=" flex flex-col gap-14 px-8 py-10">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+    <div className=" flex flex-col gap-20 px-40 py-16">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </div>
   );
 };
