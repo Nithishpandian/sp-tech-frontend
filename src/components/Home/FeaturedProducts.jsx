755fbf774312e376e5e42cabb9ceae45105ee9d7
Nithishpandian <nithishpandiyan75@gmail.com>
2023-12-23 00:29:42 +0530

    design changes

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -8,11 +8,7 @@ const FeaturedProducts = () => {
       <h1 className=" font-noto-sans font-bold text-4xl text-stone-800">
         Featured Products
       </h1>
-      <div className=" flex items-center flex-wrap gap-5 py-6 pl-6">
-        <ProductsHome image={productImg} name={"Ceramic bulb"} />
-        <ProductsHome image={productImg} name={"Ceramic bulb"} />
-        <ProductsHome image={productImg} name={"Ceramic bulb"} />
-        <ProductsHome image={productImg} name={"Ceramic bulb"} />
+      <div className=" grid grid-cols-3 items-center flex-wrap gap-8 py-6 px-14">
         <ProductsHome image={productImg} name={"Ceramic bulb"} />
         <ProductsHome image={productImg} name={"Ceramic bulb"} />
         <ProductsHome image={productImg} name={"Ceramic bulb"} />
--- src/components/Home/ProductsHome.jsx
+++ src/components/Home/ProductsHome.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const ProductsHome = ({ image, name }) => {
   return (
     <div className='flex items-center border border-[#d3d3d3] bg-[#f1f1f1] rounded-lg hover:shadow-lg duration-300 cursor-pointer'>
-        <img className=' w-24 h-20 object-cover' src={image} alt="" />
-        <p className=' font-semibold text-lg text-stone-600 font-dm-sans py-1.5 px-4'>{name}</p>
+        <img className=' w-36 h-24 object-cover' src={image} alt="" />
+        <p className=' font-semibold text-xl text-stone-600 font-dm-sans py-1.5 px-6'>{name}</p>
     </div>
   )
 }
