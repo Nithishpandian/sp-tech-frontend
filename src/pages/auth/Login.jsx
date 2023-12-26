2f038bddce28f9a13fe52de52f386c4cf6938837
Nithishpandian <nithishpandiyan75@gmail.com>
2023-12-27 01:20:17 +0530

    format login component

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import LoginComponent from '../../components/auth/LoginComponent'
+import React from "react";
+import LoginComponent from "../../components/auth/LoginComponent";
 
 const Login = () => {
   return (
-    <div className='flex justify-center items-center h-screen w-screen overflow-hidden'>
-        <LoginComponent />
+    <div className="flex justify-center items-center h-screen w-screen overflow-hidden">
+      <LoginComponent />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
